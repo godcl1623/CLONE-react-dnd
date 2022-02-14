f63a14a920f2df640acaf264a5e8627246806cde
godcl1623 <delphi5281@gmail.com>
2022-02-14 23:27:52 +0900

    crd - features - 개발 착수, 기본적인 dnd 매커니즘 구현중

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../styles/style.css';
+import ApplyDnd from './ApplyDnd';
 
 export default function App() {
   return (
     <div id='App'>
-      <h1>clone-react-dnd</h1>
+      <ApplyDnd>
+        <div id="dnd-test-zone">
+          <div id="dropzone"></div>
+          <div id="item-container">
+            <div className="item">item 1</div>
+            <div className="item">item 2</div>
+            <div className="item">item 3</div>
+          </div>
+        </div>
+      </ApplyDnd>
     </div>
   );
 }
