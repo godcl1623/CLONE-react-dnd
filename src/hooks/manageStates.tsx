5ea67d2b993716f599d35d59283e26ce5af7691a
godcl1623 <delphi5281@gmail.com>
2022-03-09 19:27:02 +0900

    crd - features - 라이브러리 패키지화 테스트

--- src/hooks/manageStates.tsx
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-// import type { TestState, AppDispatch } from '../reducers';
-
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-// export const useAppSelector: TypedUseSelectorHook<TestState> = useSelector;
